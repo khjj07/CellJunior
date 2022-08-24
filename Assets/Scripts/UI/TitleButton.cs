89e5334051ce0a6b93c633935febaca1e2a2df98
DESKTOP-Q39A3LO\KOCCA60 <goldpig622@naver.com>
2022-08-24 18:12:27 +0900

    [배성근] 게임 타이틀 버튼구현

---
+++ Assets/Scripts/UI/TitleButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleButton : MonoBehaviour
+{
+    public void BtnColor()
+    {
+        
+    }
+}
--- Assets/Scripts/UI/TitleManager.cs
+++ Assets/Scripts/UI/TitleManager.cs
@@ -2,9 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+//using UnityEngine.GameObject;
 
 public class TitleManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject GameStart;
+    [SerializeField]
+    GameObject Exit;
+    [SerializeField]
+    GameObject GameStartSlt;
+    [SerializeField]
+    GameObject ExitSlt;
+
+   public bool StartBtnActive = false;
+   public bool ExitBtnActive = false; 
 
     public void BtnStart()
     {
@@ -13,14 +25,60 @@ public class TitleManager : MonoBehaviour
 
     public void GoGameScene()
     {
+        if (StartBtnActive = true)
         SceneManager.LoadScene(1);
+        //if(ExitBtnActive = true)
+        //GameManager 에서 게임 종료 기능 호출    
+
     }
+    
+   
+
+    public void SelectStart()
+    {      
+        GameStart.SetActive(false);
+        GameStartSlt.SetActive(true);
+
+        if(ExitBtnActive = true)
+        {
+            Exit.SetActive(true);
+            ExitSlt.SetActive(false);
+        }
+       StartBtnActive = true;
+        ExitBtnActive = false;
+
+    }
+
+    public void SelectExit()
+    {
+        Exit.SetActive(false);
+        ExitSlt.SetActive(true);
+        if (StartBtnActive = true)
+        {
+            GameStart.SetActive(true);
+            GameStartSlt.SetActive(false);
+        }
+        ExitBtnActive = true;
+        StartBtnActive = false;
+
+    }
+
+    public void TitleStart()
+    {
+       // GameStart.SetActive(true);
+        GameStartSlt.SetActive(false);
+       // Exit.SetActive(true);
+        ExitSlt.SetActive(false);
+        
+    }
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
 
     // Update is called once per frame
