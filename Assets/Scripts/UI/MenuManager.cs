c6cd7ebe886d210de52c3d46440ab63461da4699
DESKTOP-Q39A3LO\KOCCA60 <goldpig622@naver.com>
2022-08-25 13:05:38 +0900

    [배성근] 파티클 이펙트, 마우스 안쓰는 게임 UI

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
@@ -20,25 +21,108 @@ public class MenuManager : MonoBehaviour
     public bool BackBAct = false;
     public bool ReStartBAct = false;
     public bool GameExitBAct = false;
+    
+    bool MeClose = false;
 
 
-    public void MenuSelect()
+    public void MenuOpen()
     {
+        if (MeClose == true)
+        {
+            this.gameObject.SetActive(true);
+            MeClose = false;
+        }
+        else if (MeClose == false)
+        {
+            this.gameObject.SetActive(false);
+            MeClose = true;
+        }
 
+    }
+
+    public void MenuSelect() //스페이스바로 선택 했을때 구현
+    {
+        if (BackB != null) // 나머지가 비활성화라면
+        {
+
+            SceneManager.LoadScene(0); // 뒤로가기
+
+        }
+        else if (ReStartB != null)
+        {
+            return; //게임 다시 시작 기능
+
+        }
+        else if (GameExitB != null)
+        {
+
+            return ; // 게임 종료 기능
+
+        }
+
+    }
+
+   public void BackB_Act()
+    {
+        BackB.SetActive(true);
+        ReStartB.SetActive(false);
+        GameExitB.SetActive(false);
+        BackBAct = true;
+        ReStartBAct = false;
+        GameExitBAct = false;
+    }
+     
+   public void ReStartB_Act()
+    {
+        BackB.SetActive(false);
+        ReStartB.SetActive(true);
+        GameExitB.SetActive(false);
+        BackBAct = false;
+        ReStartBAct = true;
+        GameExitBAct = false;
+    }
+
+   public void GameExitB_Act()
+    {
+        BackB.SetActive(false);
+        ReStartB.SetActive(false);
+        GameExitB.SetActive(true);
+        BackBAct = false;
+        ReStartBAct = false;
+        GameExitBAct = true;
     }
 
     public void UpArrow() //돌아가기 버튼이 선택중이라면 
     {
-        //if (ReStartBAct = false  GameExitBAct = false) // 나머지가 비활성화라면
-        //{
+        if (BackBAct == true) // 나머지가 비활성화라면
+        {
+            GameExitB_Act();
 
-
-
-        //}
+        }
+        else if (ReStartBAct == true)
+        {
+            BackB_Act();
+        }
+        else if (GameExitBAct == true)
+        {
+            ReStartB_Act();
+        }
     }
 
     public void DownArrow()
     {
+        if (BackBAct == true) // 나머지가 비활성화라면
+        {
+            ReStartB_Act();
+        }
+        else if (ReStartBAct == true)
+        {
+            GameExitB_Act();
+        }
+        else if (GameExitBAct == true)
+        {
+            BackB_Act();
+        }
 
     }
 
@@ -47,9 +131,19 @@ public class MenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       
+        this.gameObject.SetActive(false);
 
-    }
+        BackB.SetActive(true);
+        ReStartB.SetActive(false);
+        GameExitB.SetActive(false);
+        BackBAct = true;
+        ReStartBAct = false;
+        GameExitBAct = false;
+        
+        MeClose = true;
+
+
+}
 
     // Update is called once per frame
     void Update()
